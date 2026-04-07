9b9237b95a8f66c81a87bb1e405ed2cf7d8c8ffc
Rodrigo 'Kraby' Lira <liramailbox@gmail.com>
2026-04-07 10:39:22 -0300

    fix: added icons for SidebarLeft menus
    minor adjustments to NavItem link

--- src/components/feature/sidebar-left/SidebarLeft.tsx
+++ src/components/feature/sidebar-left/SidebarLeft.tsx
@@ -1,4 +1,16 @@
-import { FaChartPie } from "react-icons/fa";
+import {
+  FaAngleRight,
+  FaBagShopping,
+  FaChartPie,
+  FaFolder,
+  FaRegBookmark,
+  FaRegCircleUser,
+  FaRegFileLines,
+  FaRegHeart,
+  FaRegMessage,
+  FaRegNewspaper,
+  FaSliders,
+} from "react-icons/fa6";
 import SidebarHeader from "./SidebarHeader";
 import NavItem from "./NavItem";
 import type { NavItemType } from "./sidebar.types";
@@ -15,22 +27,42 @@ const sections: NavSection[] = [
   {
     id: "0",
     title: "Favorites",
-    items: [{ label: "Overview", path: "/" }],
+    items: [
+      { label: "Overview", path: "/" },
+      { label: "Projects", path: "/posts" },
+    ],
   },
   {
     id: "1",
     title: "Dashboards",
     items: [
       { label: "Overview", path: "/", icon: FaChartPie },
-      { label: "Posts", path: "/posts" },
+      { label: "eCommerce", path: "/posts", icon: FaBagShopping },
+      { label: "Projects", path: "/posts", icon: FaFolder },
     ],
   },
   {
     id: "2",
     title: "Pages",
     items: [
-      { label: "User Profile", path: "/profile" },
-      { label: "Account", path: "/account" },
+      { label: "User Profile", path: "/posts", icon: FaRegCircleUser },
+      { label: "Overview", path: "/posts", indent: true },
+      { label: "Projects", path: "/posts", indent: true },
+      { label: "Campaigns", path: "/posts", indent: true },
+      { label: "Documents", path: "/posts", indent: true },
+      { label: "Followers", path: "/posts", indent: true },
+      { label: "Account", path: "/posts", icon: FaRegBookmark },
+      { label: "Corporate", path: "/posts", icon: FaSliders },
+      { label: "Blog", path: "/posts", icon: FaRegFileLines },
+      { label: "Social", path: "/posts", icon: FaRegMessage },
+    ],
+  },
+  {
+    id: "3",
+    items: [
+      { label: "Recently", path: "/posts", icon: FaRegHeart },
+      { label: "Newsroom", path: "/posts", icon: FaRegNewspaper },
+      { label: "Quick Links", path: "/posts", icon: FaAngleRight },
     ],
   },
 ];
@@ -40,7 +72,7 @@ const SidebarLeft = () => {
     <aside className={style.sidebar}>
       <SidebarHeader />
 
-      <nav>
+      <nav className={style.nav}>
         {sections.map((section, i) => (
           <div key={i} className={style.section}>
             {section.title && <span className={style["section-title"]}>{section.title}</span>}
@@ -51,6 +83,8 @@ const SidebarLeft = () => {
           </div>
         ))}
       </nav>
+
+      <div className={style.brand}>snowUI</div>
     </aside>
   );
 };
